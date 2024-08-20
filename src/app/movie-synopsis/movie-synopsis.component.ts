1e6538478aa0ad74dd836bea571a9e18fc5713f3
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-08-20 12:23:20 +0200

    Add TypeDoc for Movie Synopsis

--- src/app/movie-synopsis/movie-synopsis.component.ts
+++ src/app/movie-synopsis/movie-synopsis.component.ts
@@ -1,12 +1,22 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+/**
+ * The `MovieSynopsisComponent` is responsible for displaying the synopsis of a movie.
+ * It is presented in a dialog when a user selects a movie to view its details.
+ */
 @Component({
   selector: 'app-movie-synopsis',
   templateUrl: './movie-synopsis.component.html',
   styleUrl: './movie-synopsis.component.scss'
 })
 export class MovieSynopsisComponent implements OnInit {
+  /**
+   * Creates an instance of `MovieSynopsisComponent`.
+   * 
+   * @param data - Data passed to the component, containing the movie title and synopsis.
+   * @param dialogRef - Reference to the dialog containing this component, used to close the dialog.
+   */
   constructor(@Inject(MAT_DIALOG_DATA)
   public data: {
     title: string,
@@ -15,8 +25,14 @@ export class MovieSynopsisComponent implements OnInit {
     public dialogRef: MatDialogRef<MovieSynopsisComponent>
   ) { }
 
+  /**
+   * Angular lifecycle hook that is called after the component's view has been fully initialized.
+   */
   ngOnInit(): void { }
 
+  /**
+   * Closes the movie synopsis dialog.
+   */
   closeDetail(): void {
     this.dialogRef.close();
   }
